16d7d149951a160e4a93ac9c497cf9b8a652ace8
yamatoto <mftb35ymt@yahoo.co.jp>
2021-04-20 19:18:09 +0900

    add HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input type="text" placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <span>TODO1</span>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <span>TODO2</span>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了したTODO</p>
+        <ul>
+          <li>
+            <span>TODO3</span>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
   );
-}
-
-export default App;
+};
