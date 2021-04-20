48da381ca8e545f3f5febcf4553cdf151e7b73a2
yamatoto <mftb35ymt@yahoo.co.jp>
2021-04-20 19:27:02 +0900

    add css

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,37 @@ import './App.css';
 export const App = (): JSX.Element => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input type="text" placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
           <li>
-            <span>TODO1</span>
-            <button>完了</button>
-            <button>削除</button>
+            <div className="list-row">
+              <span>TODO1</span>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
           </li>
           <li>
-            <span>TODO2</span>
-            <button>完了</button>
-            <button>削除</button>
+            <div className="list-row">
+              <span>TODO2</span>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了したTODO</p>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
         <ul>
           <li>
-            <span>TODO3</span>
-            <button>戻す</button>
+            <div className="list-row">
+              <span>TODO3</span>
+              <button>戻す</button>
+            </div>
           </li>
         </ul>
       </div>
