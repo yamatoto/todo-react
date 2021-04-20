8243f8e378b0b0ea9b844a8d258af2e8e31d795a
yamatoto <mftb35ymt@yahoo.co.jp>
2021-04-20 20:10:27 +0900

    impl 追加機能

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,34 @@ import React, { useState } from 'react';
 import './App.css';
 
 export const App = (): JSX.Element => {
+  const [inputText, setInputText] = useState('');
   const [incompleteTodoList, setIncompleteTodoList] = useState([
     'TODO1',
     'TODO2',
   ]);
   const [completeTodoList, setCompleteTodoList] = useState(['TODO3']);
+
+  const changeTodoTextHandler = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setInputText(event.target.value);
+
+  const addTodoHandler = () => {
+    if (inputText === '') return;
+    setIncompleteTodoList([...incompleteTodoList, inputText]);
+    setInputText('');
+  };
+
   return (
     <>
       <div className="input-area">
-        <input type="text" placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          type="text"
+          placeholder="TODOを入力"
+          value={inputText}
+          onChange={(event) => changeTodoTextHandler(event)}
+        />
+        <button onClick={addTodoHandler} disabled={inputText === ''}>
+          追加
+        </button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
